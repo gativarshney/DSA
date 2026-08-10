70abd6f162c530acb9fa7e3359cdd57b5dd07e93
Gati Varshney <171050892+gativarshney@users.noreply.github.com>
2026-08-10 22:21:52 +0530

    correction in bs

--- Binary_Search/2_Binary_Search_On_Answers/7_kth_missing.cpp
+++ Binary_Search/2_Binary_Search_On_Answers/7_kth_missing.cpp
@@ -17,7 +17,7 @@ int findKthPositive(vector<int>& arr, int k){
     int low = 0, high = n-1;
     while(low <= high){
         int mid = low + (high-low)/2;
-        int missing = arr[high] - (high+1);
+        int missing = arr[mid] - (mid+1);
         if(missing < k) low = mid+1;
         else high = mid-1;
     }
